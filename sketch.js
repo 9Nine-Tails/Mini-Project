55d1be80a26d735bc96763b6c0815e2e312efae6
Akshay P S <63052451+9Nine-Tails@users.noreply.github.com>
2021-12-03 13:06:21 +0530

    reset drum detection

--- sketch.js
+++ sketch.js
@@ -7,14 +7,15 @@ let right;
 let prev_right = 0;
 
 function preload() {
-  drum1 = loadSound("sounds/loetom.wav");
-  drum2 = loadSound("sounds/midtom.wav");
-  drum3 = loadSound("sounds/hietom.wav");
-  drum4 = loadSound("sounds/acoustic_snare.wav");
+  drum1 = loadSound(loetom);
+  drum2 = loadSound(midtom);
+  drum3 = loadSound(hietom);
+  drum4 = loadSound(acoustic_snare);
 }
 
 function setup() {
   createCanvas(640, 480);
+  frameRate(15);
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -33,63 +34,90 @@ function modelLoaded() {
 
 function draw() {
   push();
+  translate(width, 0);
   scale(-1, 1);
-  image(video, -640, 0);
+  image(video, 0, 0);
   pop();
   if (pose) {
-    if (pose.leftWrist.x < width / 4) {
-      left = 1;
-    } else if (pose.leftWrist.x > width / 4 && pose.leftWrist.x < width / 2) {
-      left = 2;
-    } else if (
-      pose.leftWrist.x > width / 2 &&
-      pose.leftWrist.x < (3 * width) / 4
-    ) {
-      left = 3;
-    } else if (pose.leftWrist.x > (3 * width) / 4) {
-      left = 4;
-    }
-    if (pose.rightWrist.x < width / 4) {
-      right = 1;
-    } else if (pose.rightWrist.x > width / 4 && pose.rightWrist.x < width / 2) {
-      right = 2;
-    } else if (
-      pose.rightWrist.x > width / 2 &&
-      pose.rightWrist.x < (3 * width) / 4
-    ) {
-      right = 3;
-    } else if (pose.rightWrist.x > (3 * width) / 4) {
-      right = 4;
-    }
-    if (left != prev_left) {
-      prev_left = left;
-      if (left == 1) {
-        drum1.play();
-      } else if (left == 2) {
-        drum2.play();
-      } else if (left == 3) {
-        drum3.play();
-      } else {
-        drum4.play();
+    // leftwrist detection
+    if (pose.leftWrist.y > (3 * height) / 4) {
+      if (pose.leftWrist.x < width / 4 && pose.leftWrist.x > 0) {
+        left = 1;
+      } else if (pose.leftWrist.x > width / 4 && pose.leftWrist.x < width / 2) {
+        left = 2;
+      } else if (
+        pose.leftWrist.x > width / 2 &&
+        pose.leftWrist.x < (3 * width) / 4
+      ) {
+        left = 3;
+      } else if (
+        pose.leftWrist.x > (3 * width) / 4 &&
+        pose.leftWrist.x < width
+      ) {
+        left = 4;
       }
-    }
-    if (right != prev_right) {
-      prev_right = right;
-      if (right == 1) {
-        drum1.play();
-      } else if (right == 2) {
-        drum2.play();
-      } else if (right == 3) {
-        drum3.play();
-      } else {
-        drum4.play();
+      if (left != prev_left) {
+        prev_left = left;
+        if (left == 1) {
+          drum1.play();
+        } else if (left == 2) {
+          drum2.play();
+        } else if (left == 3) {
+          drum3.play();
+        } else {
+          drum4.play();
+        }
       }
+    } else {
+      prev_left = 0;
     }
+    //rightwrist detection
+    if (pose.rightWrist.y > (3 * height) / 4) {
+      if (pose.rightWrist.x < width / 4 && pose.rightWrist.x > 0) {
+        right = 1;
+      } else if (
+        pose.rightWrist.x > width / 4 &&
+        pose.rightWrist.x < width / 2
+      ) {
+        right = 2;
+      } else if (
+        pose.rightWrist.x > width / 2 &&
+        pose.rightWrist.x < (3 * width) / 4
+      ) {
+        right = 3;
+      } else if (
+        pose.rightWrist.x > (3 * width) / 4 &&
+        pose.rightWrist.x < width
+      ) {
+        right = 4;
+      }
+      if (right != prev_right) {
+        prev_right = right;
+        if (right == 1) {
+          drum1.play();
+        } else if (right == 2) {
+          drum2.play();
+        } else if (right == 3) {
+          drum3.play();
+        } else {
+          drum4.play();
+        }
+      }
+    } else {
+      prev_right = 0;
+    }
+    // draw wrist location
+    push();
+    translate(width, 0);
+    scale(-1, 1);
     fill(0, 0, 255);
-    ellipse(-pose.leftWrist.x, pose.leftWrist.y, 32);
-    ellipse(-pose.rightWrist.x, pose.rightWrist.y, 32);
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 16);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 16);
+    pop();
   }
-  fill(0, 0, 255);
-  ellipse(-width/2, height/2, 32);
-  line(-width,height/2,0,height/2);
+  // trigger line
+  line(0, (3 * height) / 4, width, (3 * height) / 4);
+  line(width / 4, (3 * height) / 4, width / 4, height);
+  line(width / 2, (3 * height) / 4, width / 2, height);
+  line((3 * width) / 4, (3 * height) / 4, (3 * width) / 4, height);
 }
